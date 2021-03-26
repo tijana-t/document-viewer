65d8b9fccbb728d54a0274f3c2c505e94068319d
unknown <blazo.medenica@uhurasolutions.com>
2021-03-26 13:30:24 +0100

    document actions layout v1

--- workspace/projects/pdf-viewer/src/lib/pdf-viewer.component.ts
+++ workspace/projects/pdf-viewer/src/lib/pdf-viewer.component.ts
@@ -4,7 +4,7 @@ import { thumbnails } from './thumbnails';
 @Component({
   selector: 'lib-pdf-viewer',
   templateUrl: './pdf-viewer.component.html',
-  styles: [],
+  styleUrls: ['./styles/global.scss'],
 })
 export class PdfViewerComponent implements OnInit {
   thumbnails: { id: string; src: string }[] = [{ id: '', src: '' }];
