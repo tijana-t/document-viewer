32f5b1b296d99a68c05d5a48e8c7e9c749a9a2c2
Blaz <blazo.medenica@uhurasolutions.com>
2021-04-06 13:01:40 +0200

    fixed img layout

--- workspace/projects/pdf-viewer/src/lib/document-actions/document-actions.component.ts
+++ workspace/projects/pdf-viewer/src/lib/document-actions/document-actions.component.ts
@@ -1,5 +1,12 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { DocumentComponent } from '../document/document.component';
+import { PdfViewerComponent } from './../pdf-viewer.component';
 
 @Component({
   selector: 'lib-document-actions',
@@ -7,33 +14,35 @@ import { DocumentComponent } from '../document/document.component';
   styleUrls: ['./document-actions.component.scss'],
 })
 export class DocumentActionsComponent implements OnInit, AfterViewInit {
-  @ViewChild('document') document: DocumentComponent | undefined;
+  @Input('document') document: DocumentComponent | undefined;
   constructor() {}
 
   ngOnInit(): void {}
 
-  ngAfterViewInit() {}
+  ngAfterViewInit() {
+    console.log('DOCA', this.document?.documentImage.nativeElement);
+  }
 
   zoomIn() {
-    // let myImg = this.document?.img?.nativeElement;
-    // let currWidth = myImg.clientWidth;
-    // if (currWidth == 2500) return false;
-    // else {
-    //   return (myImg.style.width = currWidth + 100 + 'px');
-    // }
+    let myImg = this.document?.documentImage.nativeElement;
+    let currWidth = myImg.clientWidth;
+    if (currWidth == 2500) return false;
+    else {
+      return (myImg.style.width = currWidth + 100 + 'px');
+    }
   }
 
   zoomOut() {
-    // let myImg = this.document?.img?.nativeElement;
-    // let currWidth = myImg.clientWidth;
-    // if (currWidth == 100) return false;
-    // else {
-    //   return (myImg.style.width = currWidth - 100 + 'px');
-    // }
+    let myImg = this.document?.documentImage.nativeElement;
+    let currWidth = myImg.clientWidth;
+    if (currWidth == 100) return false;
+    else {
+      return (myImg.style.width = currWidth - 100 + 'px');
+    }
   }
 
   fitToPage() {
-    // let myImg = this.document?.img?.nativeElement;
-    // return (myImg.style.width = '100%');
+    let myImg = this.document?.documentImage.nativeElement;
+    return (myImg.style.width = '80%');
   }
 }
--- workspace/projects/pdf-viewer/src/lib/pdf-viewer.component.ts
+++ workspace/projects/pdf-viewer/src/lib/pdf-viewer.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 @Component({
   selector: 'lib-pdf-viewer',
   templateUrl: './pdf-viewer.component.html',
   styleUrls: ['./pdf-viewer.component.scss'],
 })
-export class PdfViewerComponent implements OnInit {
+export class PdfViewerComponent implements OnInit, AfterViewInit {
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {}
 }
