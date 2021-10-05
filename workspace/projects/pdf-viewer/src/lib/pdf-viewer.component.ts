39911b485478ac3c150978a4f305771e774d23fd
ivonav <ivonav999@gmail.com>
2021-10-05 16:03:14 +0200

    prev and next arrows

--- workspace/projects/pdf-viewer/src/lib/pdf-viewer.component.ts
+++ workspace/projects/pdf-viewer/src/lib/pdf-viewer.component.ts
@@ -13,13 +13,14 @@ import { PdfViewerService } from './pdf-viewer.service';
 import { DocumentActions } from './_config/document-actions.model';
 import { DocumentConfig } from './_config/document.model';
 import { Thumbnail } from './_config/thumbnail.model';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'lib-pdf-viewer',
   templateUrl: './pdf-viewer.component.html',
   styleUrls: ['./pdf-viewer.component.scss'],
 })
 export class PdfViewerComponent implements OnInit, AfterViewInit, OnChanges {
-  constructor(private pdfViewerService: PdfViewerService) {}
   @Input('documentImg') documentImg: string = '';
   @Input('token') token?: string = '';
   @Input('thumbnails') thumbnails: Thumbnail[] = [{ id: '', src: '' }];
@@ -32,12 +33,25 @@ export class PdfViewerComponent implements OnInit, AfterViewInit, OnChanges {
     containerHeight: 0,
     containerWidth: 0,
   };
-  @Input('docName') docName: string = '';
-  @Input('docDate') docDate: string = '';
   @Input('docModel') docModel: string= '';
+  @Input('params') params: any;
+  @Input('singleDocument') singleDocument: any;
+  @Input('inProjects') inProjects: any;
 
+  docName: string = '';
+  docDate: string = '';
+  private url : string = '';
 
-  ngOnInit() {}
+  constructor(
+    private pdfViewerService: PdfViewerService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.url = this.router.url;
+    this.docName = this.singleDocument.file.originalName;
+    this.docDate = this.singleDocument.file.createdAt.toString();
+    
+  }
 
   ngAfterViewInit() {}
 
@@ -54,6 +68,49 @@ export class PdfViewerComponent implements OnInit, AfterViewInit, OnChanges {
     if (changes['documentConfig'] && changes['documentConfig'].currentValue) {
       this.documentConfig = changes['documentConfig'].currentValue;
     }
+  }
+
+  changeDoc(status: boolean){
+
+    this.docName = this.singleDocument.file.originalName;
+    this.docDate = this.singleDocument.file.createdAt.toString();
+if(!this.inProjects){
+    if (status) {
+      this.router.navigateByUrl(
+        `/training/models/${this.url.split('/')[3]}/${this.params.modelId}/${
+          this.url.split('/')[5]
+        }/${this.singleDocument.next.id}/${this.singleDocument.next.fileName}/1/`
+      );
+      this.docName = this.singleDocument.next.name;
+    this.docDate = this.singleDocument.next.date.toString();
+    } else {
+      this.router.navigateByUrl(
+        `/training/models/${this.url.split('/')[3]}/${this.params.modelId}/${
+          this.url.split('/')[5]
+        }/${this.singleDocument.prev.id}/${this.singleDocument.prev.fileName}/1/`
+      );
+      this.docName = this.singleDocument.prev.name;
+    this.docDate = this.singleDocument.prev.date.toString();
+    }
+  } else {
+    if (status) {
+      this.router.navigateByUrl(
+        `/projects/viewer/${this.url.split('/')[3]}/${this.url.split('/')[4]}/${
+          this.singleDocument.next.id
+        }/${this.singleDocument.next.fileName}/1/`
+      );
+      this.docName = this.singleDocument.next.name;
+    this.docDate = this.singleDocument.next.date.toString();
+    } else {
+      this.router.navigateByUrl(
+        `/projects/viewer/${this.url.split('/')[3]}/${this.url.split('/')[4]}/${
+          this.singleDocument.prev.id
+        }/${this.singleDocument.prev.fileName}/1/`
+      );
+      this.docName = this.singleDocument.prev.name;
+    this.docDate = this.singleDocument.prev.date.toString();
+    }
+  }
   }
   
 }
