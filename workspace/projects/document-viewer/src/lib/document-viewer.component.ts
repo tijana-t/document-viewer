c399fbe5e34331ae999e4f201ba0e995fb633c41
grungert <bojan.atman@gmail.com>
2022-02-10 11:22:16 +0100

    sub doc list in viewer

--- workspace/projects/document-viewer/src/lib/document-viewer.component.ts
+++ workspace/projects/document-viewer/src/lib/document-viewer.component.ts
@@ -14,11 +14,30 @@ import { DocumentActions } from './_config/document-actions.model';
 import { SearchResult } from './_config/document-search.model';
 import { DocumentConfig } from './_config/document.model';
 import { Thumbnail } from './_config/thumbnail.model';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'ngx-view-document',
   templateUrl: './document-viewer.component.html',
   styleUrls: ['./document-viewer.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ width: 25, opacity: 0 }),
+        animate('0.3s ease-out', style({ width: 'auto', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ width: 'auto', opacity: 1 }),
+        animate('0.3s ease-in', style({ width: 25, opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class DocumentViewerComponent
   implements OnInit, AfterViewInit, OnChanges
@@ -45,17 +64,18 @@ export class DocumentViewerComponent
   @Input('documentConfig') documentConfig: DocumentConfig = {
     containerWidth: 0,
   };
-  @Input('docModel') docModel: string = '';
+  @Input('docModel') docModel: any;
   @Input('params') params: any;
   @Input('singleDocument') singleDocument: any;
   @Input('inProjects') inProjects: any;
   @Output('naturalDimensions') naturalDimensions = new EventEmitter();
   subscriptions = new Subscription();
   destroy$ = new Subject();
+  collapsStatus = false;
   ngOnInit() {
     this.subscriptions = this.docViewerService.lineStatus.subscribe(
       (status) => {
-          this.linePosition.emit();
+        this.linePosition.emit();
       }
     );
   }
@@ -87,13 +107,60 @@ export class DocumentViewerComponent
     this.downloadDocumentEvent.emit(event);
   }
 
+  collapsSubDocs() {
+    if (!this.collapsStatus) {
+      for (let i = 0; i < this.singleDocument.file.matchingDocs.length; i++) {
+        setTimeout(() => {
+          this.singleDocument.file.matchingDocs[i].collaps = true;
+        }, 100 * (i + 1));
+      }
+    } else {
+      for (
+        let i = this.singleDocument.file.matchingDocs.length - 1;
+        i > -1;
+        i--
+      ) {
+        setTimeout(() => {
+          this.singleDocument.file.matchingDocs[i].collaps = false;
+        }, Math.abs(100 * (i - this.singleDocument.file.matchingDocs.length + 1)));
+      }
+    }
+    this.collapsStatus = !this.collapsStatus;
+  }
+
+  getFileTypeForHipotekarnaSub(data: any) {
+    if (
+      (this.docModel.modelContainerId === '60e448112eaba436d16da8d9' ||
+        this.docModel.modelContainerId === '61091c067e9ae6f8779d131c' ||
+        this.docModel.modelContainerId === '61dbe46ea61675eb2f95c632' ||
+        this.docModel.modelContainerId === '61dc181da61675eb2f98401b') &&
+      data.length !== 0 &&
+      data[0].groupsList[0].intents.length !== 0 &&
+      data[0].groupsList[0].intents[0].intentsList.length !== 0 &&
+      data[0].groupsList[0].intents[0].intentsList[0].entities.length !== 0 &&
+      data[0].groupsList[0].intents[0].intentsList[0].entities[0].entitiesList
+        .length !== 0
+    ) {
+      const entValue =
+        data[0].groupsList[0].intents[0].intentsList[0].entities[0]
+          .entitiesList[0].value;
+      if (entValue !== '') {
+        return entValue;
+      } else {
+        return 'N/A';
+      }
+    } else {
+      return 'N/A';
+    }
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes['pageInfo'] && changes['pageInfo'].currentValue) {
       this.docViewerService.pageInfo.next(changes['pageInfo'].currentValue);
     }
     if (changes['documentConfig'] && changes['documentConfig'].currentValue) {
       this.documentConfig = changes['documentConfig'].currentValue;
-      this.docViewerService.docConfSubject.next(this.documentConfig)
+      this.docViewerService.docConfSubject.next(this.documentConfig);
     }
     if (changes['searchResult'] && changes['searchResult'].currentValue) {
       this.docViewerService.searchResultSubject.next(
@@ -105,6 +172,9 @@ export class DocumentViewerComponent
     }
     if (changes['singleDocument'] && changes['singleDocument'].currentValue) {
       this.singleDocument = changes['singleDocument'].currentValue;
+    }
+    if (changes['docModel']) {
+      this.docModel = changes['docModel'].currentValue;
     }
   }
 
